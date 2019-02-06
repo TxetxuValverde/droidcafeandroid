eac6c33fd8cd5184f2d6dfc31945c05c1792893a
Javier Lete <jglete@eskura21.com>
2019-02-06 20:19:15 +0100

    Menús

---
+++ app/src/main/java/com/example/droidcafe/DatePickerFragment.java
@@ -0,0 +1,32 @@
+package com.example.droidcafe;
+
+
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.DialogFragment;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import android.widget.DatePicker;
+import android.widget.TextView;
+
+public class DatePickerFragment extends DialogFragment
+        implements DatePickerDialog.OnDateSetListener {
+
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
+        return new DatePickerDialog(getActivity(), this, 2010, 1, 2);
+    }
+
+    @Override
+    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+        MainActivity activity = (MainActivity) getActivity();
+        activity.processDatePickerResult(year, month, dayOfMonth);
+    }
+}
--- app/src/main/java/com/example/droidcafe/MainActivity.java
+++ app/src/main/java/com/example/droidcafe/MainActivity.java
@@ -1,17 +1,26 @@
 package com.example.droidcafe;
 
+import android.app.DatePickerDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.DialogFragment;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.ContextMenu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.DatePicker;
+import android.widget.ImageView;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+        implements DatePickerDialog.OnDateSetListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +28,9 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        ImageView iv = findViewById(R.id.donut);
+        registerForContextMenu(iv);
 
 //        FloatingActionButton fab = findViewById(R.id.fab);
 //        fab.setOnClickListener(new View.OnClickListener() {
@@ -28,6 +40,26 @@ public class MainActivity extends AppCompatActivity {
 //                        .setAction("Action", null).show();
 //            }
 //        });
+    }
+
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+
+        switch (item.getItemId()) {
+            case R.id.contextual2:
+                displayToast("Pulsado el contexto2");
+                return true;
+            default:
+                return super.onContextItemSelected(item);
+        }
+
+    }
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_context, menu);
     }
 
     @Override
@@ -45,11 +77,56 @@ public class MainActivity extends AppCompatActivity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        switch (id) {
+            case R.id.action_settings:
+                AlertDialog.Builder myAlertBuilder = new
+                        AlertDialog.Builder(MainActivity.this);
+                myAlertBuilder.setTitle("Alert");
+                myAlertBuilder.setMessage("Click OK to continue, or Cancel to stop:");
+                // Add the dialog buttons.
+                myAlertBuilder.setPositiveButton("OK", new
+                        DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int which) {
+                                // User clicked OK button.
+                                Toast.makeText(getApplicationContext(), "Pressed OK",
+                                        Toast.LENGTH_SHORT).show();
+                            }
+                        });
+                myAlertBuilder.setNegativeButton("Cancel", new
+                        DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int which) {
+                                // User cancelled the dialog.
+                                Toast.makeText(getApplicationContext(), "Pressed Cancel",
+                                        Toast.LENGTH_SHORT).show();
+                            }
+                        });
+                myAlertBuilder.setNeutralButton("Neutral", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Toast.makeText(getApplicationContext(), "Pressed Neutral",
+                                Toast.LENGTH_SHORT).show();
+                    }
+                });
+                myAlertBuilder.show();
+                return true;
+            case R.id.action_settings2:
+                DialogFragment newFragment = new DatePickerFragment();
+                newFragment.show(getSupportFragmentManager(),"datePicker");
+                return true;
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void processDatePickerResult(int year, int month, int day) {
+        String month_string = Integer.toString(month+1);
+        String day_string = Integer.toString(day);
+        String year_string = Integer.toString(year);
+        String dateMessage = (day_string +
+                "/" + month_string + "/" + year_string);
+
+        Toast.makeText(this, "Date: " + dateMessage,
+                Toast.LENGTH_SHORT).show();
     }
 
     public void displayToast(String message) {
@@ -71,5 +148,10 @@ public class MainActivity extends AppCompatActivity {
     public void onCartPress(View view) {
         Intent intent = new Intent(this, OrderActivity.class);
         startActivity(intent);
+    }
+
+    @Override
+    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+
     }
 }
